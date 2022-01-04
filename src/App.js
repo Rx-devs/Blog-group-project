348826039d394a7f71f1435e498de8bdc9e8b5b7
MohammadSiam <siamk417@gmail.com>
2022-01-04 15:31:48 +0600

    Carrer

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import Home from './Pages/Home/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome Home Here</h1>
+      <p>Go Everywhere</p>
+      <h5>Blog website</h5>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
