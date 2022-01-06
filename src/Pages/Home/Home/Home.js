0e8c495846fba24a5a0dd37bfd793f525802a4b9
Tonmoy Roy <rxdevs100@gmail.com>
2022-01-06 12:23:31 +0600

    demo removed

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
-import Career from '../Career/Career';
 
 const Home = () => {
     return (
         <div>
             <Navigation></Navigation>
-            <Career></Career>
             <Footer></Footer>
         </div>
     );
