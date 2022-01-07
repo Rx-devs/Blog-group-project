4dbb39d20bd7a593723a3fd9edab911c0e3e621c
Tonmoy Roy <rxdevs100@gmail.com>
2022-01-07 13:18:46 +0600

    carrier blogs and news section added

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import Footer from '../../Shared/Footer/Footer';
 import Navigation from '../../Shared/Navigation/Navigation';
+import ProgrammingBlog from '../Blogs/ProgrammingBlog';
 import Careers from '../Careers/Careers';
 import Newses from '../Newses/Newses';
-import ProgrammingBlog from '../Blogs/ProgrammingBlog';
-import Carriers from '../Carriers/Carriers';
+// import Carriers from '../Carriers/Carriers';
 
 const Home = () => {
     return (
